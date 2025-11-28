a379fb8fa537a7f2cc7921d1b7bcebd351fbb1a3
jessicatinguely <jessicatinguely@icloud.com>
2025-11-28 10:40:40 -0300

    :sparkles: Correção impar

--- src/processar_palindromos/ProcessarPalindromos.java
+++ src/processar_palindromos/ProcessarPalindromos.java
@@ -5,54 +5,49 @@ import java.util.Scanner;
 public class ProcessarPalindromos {
 
 	public static void main(String[] args) {
-	
-	//Receber a string 
-	
-	Scanner leia = new Scanner(System.in);
-	
-	String palavra;
-	
-	System.out.println("Digite uma palavra: ");
-	palavra= leia.nextLine();
+
+		// Receber a string
+		Scanner leia = new Scanner(System.in);
+
+		String palavra;
+
+		System.out.println("Digite uma palavra: ");
+		palavra = leia.nextLine();
+
+		// Fazer o lenght
+		int tamanhoPalavra = palavra.length();
+
+		// Validar se é maior que 3 e se é impar
+		if (tamanhoPalavra < 3) {
+			System.out.println("Não é um Palíndromo");
+			return; // encerra o programa aqui
+		}
 		
-	//Fazer o lenght 
-	
-	int tamanhoPalavra = palavra.length();
-		
-	//Validar se é maior que 3 e se é impar 
-    
-    if (tamanhoPalavra < 3) {
-    	System.out.println("Não é um Palíndromo");
-    	return; // encerra o programa aqui
-    }
-   
-    //Vetor da palavra
-    char[] letras = palavra.toCharArray();
-    
-    //Vetor Invertido 
-    char[] invertida = new char[tamanhoPalavra];
-    for (int i = 0; i < tamanhoPalavra; i++) {
-        invertida[i] = letras[tamanhoPalavra - 1 - i];
-    }
+		// Vetor da palavra
+		char[] letras = palavra.toCharArray();
 
-    //Comparar se é palíndromo
-    boolean palindromo = true;
+		// Vetor Invertido
+		char[] invertida = new char[tamanhoPalavra];
+		for (int i = 0; i < tamanhoPalavra; i++) {
+			invertida[i] = letras[tamanhoPalavra - 1 - i];
+		}
 
-    for (int i = 0; i < tamanhoPalavra; i++) {
-        if (letras[i] != invertida[i]) {
-            palindromo = false;
-            break;
-        }
-    }
+		// Comparar se é palíndromo
+		boolean palindromo = true;
 
-    //Mostrar resultado
-    if (palindromo) {
-        System.out.println("É um palíndromo!");
-    } else {
-        System.out.println("Não é um palíndromo!");
-    }
-    
-    leia.close();
-	
+		for (int i = 0; i < tamanhoPalavra; i++) {
+			if (letras[i] != invertida[i]) {
+				palindromo = false;
+				break;
+			}
+		}
+
+		// Mostrar resultado
+		if (palindromo) {
+			System.out.println("É um palíndromo!");
+		} else {
+			System.out.println("Não é um palíndromo!");
+		}
+		leia.close();
 	}
 }
